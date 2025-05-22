c73699b264368dce01b512fb4a76bc3d0d6d0908
ATH <alex.t.hoffmann@icloud.com>
2025-05-22 19:08:25 +0200

    update version

--- src/sanity/live.ts
+++ src/sanity/live.ts
@@ -2,7 +2,7 @@ import { defineLive } from 'next-sanity'
 import { client } from '@/sanity/client'
 
 export const { sanityFetch, SanityLive } = defineLive({
-  client: client.withConfig({ apiVersion: 'vX' }),
+  client: client.withConfig({ apiVersion: '2024-11-01' }),
   serverToken: process.env.SANITY_API_TOKEN,
   browserToken: process.env.SANITY_API_TOKEN,
 })
