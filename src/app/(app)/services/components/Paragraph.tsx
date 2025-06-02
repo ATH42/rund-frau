a1d70ebf5587a4ced08d8f0a7f2e5a63c28bf54c
ATH <alex.t.hoffmann@icloud.com>
2025-06-02 09:24:19 +0200

    asda

--- src/app/(app)/services/components/Paragraph.tsx
+++ src/app/(app)/services/components/Paragraph.tsx
@@ -2,7 +2,11 @@ export function Paragraph({ title, content }: { title: string; content: string }
   return (
     <div className="p-5 justify-center items-center rounded-lg shadow-none text-primary-darker">
       <h3 className="font-bold wrap-break-word pb-6">{title}</h3>
-      <p>{content}</p>
+      <p>
+        {content.split('\n').map((line, i) => (
+          <p key={i}>{line}</p>
+        ))}
+      </p>
     </div>
   )
 }
