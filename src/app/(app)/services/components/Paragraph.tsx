2359ed6acadb347722fe63030a51288522d1736c
ATH <alex.t.hoffmann@icloud.com>
2025-06-02 09:30:13 +0200

    clean

--- src/app/(app)/services/components/Paragraph.tsx
+++ src/app/(app)/services/components/Paragraph.tsx
@@ -1,12 +1,16 @@
 export function Paragraph({ title, content }: { title: string; content: string }) {
+  const normalizedContent = content.replace(/\\n/g, '\n')
+
   return (
     <div className="p-5 justify-center items-center rounded-lg shadow-none text-primary-darker">
-      <h3 className="font-bold wrap-break-word pb-6">{title}</h3>
-      <p>
-        {content.split('\n').map((line, i) => (
-          <p key={i}>{line}</p>
+      <h3 className="font-bold break-words pb-6">{title}</h3>
+      <div>
+        {normalizedContent.split('\n').map((line, i) => (
+          <p key={i} className="mb-4">
+            {line}
+          </p>
         ))}
-      </p>
+      </div>
     </div>
   )
 }
